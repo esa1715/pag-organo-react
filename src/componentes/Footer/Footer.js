39b34604f14ac23990e3aca24061d0efecb2e05e
esa1715 <silvalveserik1@gmail.com>
2025-02-23 12:32:44 -0300

    Primeiro commit

---
+++ src/componentes/Footer/Footer.js
@@ -0,0 +1,10 @@
+import './Footer.css'
+
+export const Footer = () => {
+    //JSX
+    return (
+        <footer className="footer">
+        <img src="/imagens/footer.png" alt="O rodapé da página do Organo"/>
+        </footer>
+    )
+}
